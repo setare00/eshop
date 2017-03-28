39a36c44c9ae14b8f6e98d17886ed5e07a8a93b5
setare00 <setarehadavi@yahoo.com>
2017-03-27 23:16:34 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,78 +1,9 @@
 <?php
 include("includes/header.php");
+include_once("index.html");
 ?>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="images/0601_homepage.3.jpg" alt="Image">
-        <div class="carousel-caption">
-          <h3>Sell $</h3>
-          <p>Money Money.</p>
-        </div>
-      </div>
 
-      <div class="item">
-        <img src="images/0607_M3.jpg" alt="Image">
-        <div class="carousel-caption">
-          <h3>More Sell $</h3>
-          <p>Lorem ipsum...</p>
-        </div>
-      </div>
-    </div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-</div>
-
-  <div >
-    
-  </div>
-<div class="container text-center">
-  <h3></h3><br>
-  <div class="row">
-    <div class="col-sm-4">
-      <a href="clothes.php"> <img src="images/clothing.jpg" class="img-responsive" style="width:100%" alt="Image"></a></br>
-      <p style="font-size: large"><b>CLOTHES</b></p>
-    </div>
-    <div class="col-sm-4">
-      <a href="bags.php"><img src="images/Bag_1024x1024.jpg" class="img-responsive" style="width:100%" alt="Image"></a><br/>
-      <p style="font-size: large"><b>BAG</b></p>
-    </div>
-    <div class="col-sm-4">
-      <img src="images/tumblr_static_best_fashion_accessories.jpg" class="img-responsive" style="width:100%" alt="Image"></br>
-      <p style="font-size: large"><b>ACCESSORIES</b> </p>
-    </div>
-    <!-- <div class="col-sm-4">
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-    </div> -->
-
-  </div>
-</div><br>
-
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
-
-</body>
-</html>
 
